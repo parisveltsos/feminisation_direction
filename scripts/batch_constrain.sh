8698c63bf4499f564c12067b060635dffc299ea7
Paris Veltsos <parisveltsos@gmail.com>
2021-01-13 22:33:02 -0600

    Added information on generating data for Wiberg21

--- scripts/batch_constrain.sh
+++ scripts/batch_constrain.sh
@@ -1,3 +1,4 @@
+# Used in Veltsos 2017
 Rscript ~/git/feminisation_direction/scripts/constrain.r E M M C B 0.1
 Rscript ~/git/feminisation_direction/scripts/constrain.r E M M C H 0.1
 Rscript ~/git/feminisation_direction/scripts/constrain.r E M F C B 0.1
@@ -6,3 +7,9 @@ Rscript ~/git/feminisation_direction/scripts/constrain.r E M M V B 0.1
 Rscript ~/git/feminisation_direction/scripts/constrain.r E M M V H 0.1
 Rscript ~/git/feminisation_direction/scripts/constrain.r E M F V B 0.1
 Rscript ~/git/feminisation_direction/scripts/constrain.r E M F V H 0.1
+
+# Used in Wiberg 2021
+Rscript ~/git/feminisation_direction/scripts/constrain.r E M M All B 0.1
+Rscript ~/git/feminisation_direction/scripts/constrain.r E M M All H 0.1
+Rscript ~/git/feminisation_direction/scripts/constrain.r E M F All B 0.1
+Rscript ~/git/feminisation_direction/scripts/constrain.r E M F All H 0.1
